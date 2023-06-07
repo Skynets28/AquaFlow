bd1337841284d8a60b6708b8a0cad265644dba85
skynets <semoe911@gmail.com>
2023-06-07 05:44:43 -0600

    Modificacion en ciclo do-while y en la webpage

--- servidor-tanque-de-llenado.cpp
+++ servidor-tanque-de-llenado.cpp
@@ -87,36 +87,8 @@ void clientRequest( ) { /* funcion clientRequest */
         //Obtencion de la medida del sensor
         medida = slaveState.toInt();
         //Porcentaje de llenado del tanque
-        porcentaje = ((medida-20)*100)/capacidadDelTanque;
+        porcentaje = (((medida)*100)/capacidadDelTanque);
         Serial.print("\nPorcentaje de llenado = ");Serial.print(porcentaje); Serial.print("%");
-        //Encendido o apagado de la bomba
-        if (porcentaje >= 80) {
-          digitalWrite(BOMBA, HIGH); // apaga la bomba
-          contador = 0;
-          Serial.println(contador);
-        } else {
-          digitalWrite(BOMBA, LOW);  // enciende la bomba
-          contador++;
-          Serial.println(contador);
-        }
-        if (contador >= 60){
-            //delay(120000);
-            medida2 = slaveState.toInt();
-          if((medida2 - medida)<15){
-            //delay(120000);
-            digitalWrite(BOMBA, HIGH);  // Apaga la bomba
-            do{
-              delay(1000);
-              contador2++;
-              Serial.println(contador2);
-            }while (contador2<=60);
-            contador2 = 0;
-          }
-          contador=0;
-        }
-        webpage(client, porcentaje);
-        delay(1000);
-
         client.print(nom);
         if (sendCmd) {
           sendCmd = false;
@@ -133,6 +105,71 @@ void clientRequest( ) { /* funcion clientRequest */
       }
     }
   }
+  
+            //Encendido o apagado de la bomba
+        if (porcentaje >= 90) {
+          digitalWrite(BOMBA, HIGH); // apaga la bomba
+          contador = 0;
+          Serial.println(contador);
+        } else {
+          digitalWrite(BOMBA, LOW);  // enciende la bomba
+          contador++;
+          Serial.println(contador);
+        }
+    if (contador >= 60){
+            //delay(120000);
+            medida2 = slaveState.toInt();
+          if((medida2 - medida)<15){
+            //delay(120000);
+            digitalWrite(BOMBA, HIGH);  // Apaga la bomba
+            do{
+              delay(1000);
+              contador2++;
+              Serial.println(contador2);
+              ////Revisa si el cliente esta conectado
+              WiFiClient client = server.available();
+              client.setTimeout(50);
+              if (client) {
+                if (client.connected()) {
+                  //Imprime la direccion ip del cliente
+                  Serial.print(" ->");Serial.println(client.remoteIP());
+                  String request = client.readStringUntil('\r'); //receive el mensaje del cliente
+                  
+                  if (request.indexOf("Slave0") == 0) {
+                    //Manejo de la solicitud del esclavo
+                    Serial.print("From "); Serial.println(request);
+                    int index = request.indexOf(":");
+                    String slaveid = request.substring(0, index);
+                    slaveState = request.substring(request.indexOf("x") + 1, request.length());
+                    Serial.print("state received: "); Serial.println(slaveState);
+                    //Obtencion de la medida del sensor
+                    medida = slaveState.toInt();
+                    //Porcentaje de llenado del tanque
+                    porcentaje = (((medida)*100)/capacidadDelTanque);
+                    Serial.print("\nPorcentaje de llenado = ");Serial.print(porcentaje); Serial.print("%");
+                    client.print(nom);
+                    if (sendCmd) {
+                      sendCmd = false;
+                      client.println(": Ok " + slaveid + "! Set state to x" + String(slaveCmd) + "\r");
+                    } else {
+                      client.println(": Hi " + slaveid + "!\r"); // sends the answer to the client
+                    }
+                    client.stop();                // terminates the connection with the client
+                  } else {
+                    Serial.print("From Browser : "); Serial.println(request);
+                    client.flush();
+                    //handleRequest(request);
+                    webpage(client, porcentaje);
+                  }
+                }
+              }
+            }while (contador2<=60);
+            contador2 = 0;
+          }
+          contador=0;
+        }
+        delay(1000);
+
 }
 
 void webpage(WiFiClient browser, float porcentaje) { /* function webpage */
@@ -145,7 +182,7 @@ void webpage(WiFiClient browser, float porcentaje) { /* function webpage */
   browser.println("<!DOCTYPE HTML>");
   browser.println("<html>");
   browser.println("<head>");
-  browser.println("<meta charset=\"UTF-8\"><meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><title>Sistema de agua</title>/div>");
+  browser.println("<meta charset=\"UTF-8\"><meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><title>Sistema de agua</title>");
   browser.println("<style>");
   browser.println("*{padding:0;margin:0;}header{background-color:#38b6ff;text-align:center;height:60px;width:100%;}body{font-family:system-ui,-apple-system,BlinkMacSystemFont,'Segoe UI',Roboto,Oxygen,Ubuntu,Cantarell,'Open Sans','Helvetica Neue',sans-serif;background-color:black;}div{display:flex;height:80vh;width:100vw;align-items:center;justify-content:center;}");
   browser.println("h3{color:white;text-align:center;}h2{font-size:7rem;position:absolute;}.border{color:white;text-shadow:-1px -1px 0 white,1px -1px 0 white,1px 1px 0 white,-1px 1px 0 white,;}.wave{color:#09f;animation:wave 3s ease-in-out infinite;}@keyframes wave{0%,100%{clip-path:polygon(0% 47%,10% 48%,33% 54%,54% 60%,70% 61%,84% 59%,100% 52%,100% 100%,0% 100%);}50%{clip-path:polygon(0% 60%,15% 65%,34% 66%,51% 62%,67% 50%,84% 45%,100% 46%,100% 100%,0% 100%);}}");
